3a7fd4f5d401a916cecf59f923918e20539203b7
Tan Ki In <70200450+Cycerotki@users.noreply.github.com>
2022-11-10 21:30:10 +0800

    uploading some files

---
+++ test/test_rsa.js
@@ -0,0 +1,15 @@
+var forge = require('node-forge');
+
+/*  Use line 6 to randomly create a key
+    Use line 7 to test AES-128 example vector
+*/
+var rsa = forge.pki.rsa
+            var keypair = rsa.generateKeyPair({bits: 1024, e:0x10011})
+            var n = (keypair.publicKey.n).toString()
+            var e = (keypair.publicKey.e).toString()
+            const rsaPrivateKey = keypair.privateKey
+
+            //console.log(keypair.publicKey)
+            //console.log(keypair.privateKey)
+            console.log(n)
+            console.log(e)
